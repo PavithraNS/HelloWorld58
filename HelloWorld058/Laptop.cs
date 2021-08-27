969e0ef26d6a687db493f28dc369ede8c44cbe64
PavithraNS <nspavi1991@gmail.com>
2021-08-27 09:44:05 +0530

    [Pavithra] Add newProject

---
+++ HelloWorld058/Laptop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld058
+{
+    class Laptop
+    {
+        string name;
+        float size;
+
+        public void Run()
+        {
+            Console.WriteLine("Running");
+        }
+    }
+}
---
+++ HelloWorld058/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld058
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Test();
+            Laptop laptop = new Laptop();
+            laptop.Run();
+            Console.Read();
+        }
+        /// <summary>
+        /// Tests this instance.
+        /// </summary>
+        public static void Test()
+        {
+            Console.WriteLine("Testing");
+        }
+    }
+}
